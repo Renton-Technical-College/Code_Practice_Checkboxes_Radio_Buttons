01bb37fc8c0323e7339e3641307124e53b96cc49
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-11-13 16:25:13 -0800

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Code_Practice_Checkboxes_Radio_Buttons
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        } // Form1()
+
+        // In this exercise, you will practice working with your checkboxes and radio buttons.
+
+        // Create a winform that has
+
+        // 1 Textbox: For the name of the sandwich
+
+        // 3 Group Boxes: 1 for each set of radio buttons, and 1 for your toppings. Make sure they display the correct name of the item.
+
+        // 2 Sets of Radio buttons:
+
+        //        Set 1: Bread - 4 Options.You can use the ones I provided or choose your own
+
+        //        Set 2: Heat - 4 Options.You can make up your own or the ones I provide
+
+        // 1 Set of Checkboxes for Toppings - Give 8 toppings
+
+        // 1 Rich Text Box to display the sandwich information
+
+        // 1 Label that is used to display the name of the sandwich 
+
+        // 1 Button that displays all the information.
+
+        // See the assignment page for an animated gif of the expected result
+
+        // You can design it however you like
+
+
+
+    } // class
+
+} // namespace
---
+++ README.md
@@ -0,0 +1,2 @@
+# Code_Practice_Checkboxes_Radio_Buttons
+ Programming 122 - Checkboxes and Radio Buttons
